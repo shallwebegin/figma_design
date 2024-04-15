21402fd55083f40c7f6fad955bb7a40398fdb2ba
Berke Kartal <shallwebegin@users.noreply.github.com>
2024-04-15 11:38:55 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/figma_design/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.figma_design
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/response/api_response.dart
@@ -0,0 +1,17 @@
+import 'package:figma_design/data/response/status.dart';
+
+class ApiResponse<T> {
+  Status? status;
+  T? data;
+  String? message;
+
+  ApiResponse(this.status, this.data, this.message);
+
+  ApiResponse.loading() : status = Status.LOADING;
+  ApiResponse.completed(this.data) : status = Status.LOADING;
+  ApiResponse.error(this.message) : status = Status.ERROR;
+
+  String toString() {
+    return 'Status : $status \n Message : $message \n Data: $data';
+  }
+}
